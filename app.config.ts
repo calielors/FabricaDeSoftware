18ca483333a14a270c831b9bfd4a7e7259d7f2c5
Alec S. <alecbittencourt@hotmail.com>
2026-06-24 22:59:25 -0300

    Build fix test 3

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,67 @@
-import appJson from "./app.json";
-
 export default {
-  ...appJson,
   expo: {
-    ...appJson.expo,
+    name: "AgendaSUS",
+    slug: "app-agendasus",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "agendasus",
+    userInterfaceStyle: "automatic",
+
+    splash: {
+      image: "./assets/images/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
+
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.anonymous.AgendaSUS",
+    },
+
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      predictiveBackGestureEnabled: false,
+      package: "com.anonymous.AgendaSUS",
+      intentFilters: [
+        {
+          action: "VIEW",
+          autoVerify: true,
+          data: [
+            {
+              scheme: "https",
+              host: "app.example.com",
+            },
+          ],
+          category: ["BROWSABLE", "DEFAULT"],
+        },
+      ],
+    },
+
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+
+    plugins: ["expo-router", "expo-font", "expo-web-browser"],
+
+    experiments: {
+      typedRoutes: true,
+    },
+
     extra: {
-      ...appJson.expo.extra,
+      router: {},
+      eas: {
+        projectId: "d2e28433-db91-47eb-b75e-4fd3463eb08a",
+      },
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     },
+
+    owner: "agendasus",
   },
 };
