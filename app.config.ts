2410758dba9f00a02a382a647e2212f45f7452e5
Alec S. <alecbittencourt@hotmail.com>
2026-06-24 22:40:20 -0300

    Build fix test 2

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,11 @@
+import appJson from "./app.json";
+
 export default {
+  ...appJson,
   expo: {
+    ...appJson.expo,
     extra: {
+      ...appJson.expo.extra,
       supabaseUrl: process.env.SUPABASE_URL,
       supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
     },
